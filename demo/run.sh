274e55a5804668f1b4f88f1be34eb2a3761edca6
Joshua Nussbaum <67082011+joshuanussbaum@users.noreply.github.com>
2023-06-27 12:37:52 -0500

    Prep run.sh for main pull

--- demo/run.sh
+++ demo/run.sh
@@ -25,12 +25,12 @@ echo "Generating social media posts..."
 python3 generate_posts.py
 
 # add user accounts to target system
-# echo "Adding honeyBOTs to target system..."
-# python3 add_users.py $server
+echo "Adding honeyBOTs to target system..."
+python3 add_users.py $server
 
 # mkdir -p ./Auto-GPT/autogpt/auto_gpt_workspace/output
 # cp -R ./output/ ./Auto-GPT/autogpt/auto_gpt_workspace/output
 
-# # # TODO: run Auto-GPT and have a bot select a login json from the output folder, then attempt to log in with the provided username and each of the provided passwords, then log the results to a file. 
-
+#  # TODO: run Auto-GPT and have a bot select a login json from the output folder, then attempt to log in with the provided username and each of the provided passwords, then log the results to a file. 
+# echo "Running Auto-GPT, time for you to take over!s"
 # cd Auto-GPT && docker-compose run --rm auto-gpt # at this point you'll need to do SOME intervention but eventually i'll automate it
